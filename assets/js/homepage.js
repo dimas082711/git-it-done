8b0f5d59fe0d81cea72a75e3c409ad5fa88f0bb5
dimas <dprcontractor@gmail.com>
2022-01-28 19:38:19 -0500

    add language buttons to the page

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,7 +1,22 @@
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonEl = document.querySelector(" #language-buttons")
 
 
+var getFeaturedRepos = function(language) {
+var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+fetch(apiUrl).then(function(response){
+    if (response.ok) {
+        response.json().then(function(data){
+            displayRepos(data.items, language);
+        })
+        
+    } else {
+        alert('Error: GitHub User Not Found');
+    }
+})
+}
 
 var getUserRepos = function(user) {
 // format the github api url
@@ -87,5 +102,15 @@ var displayRepos = function(repos, searchTerm) {
     }
 }
 
+var buttonClickHandler = function(event) {
+var language = event.target.getAttribute("data-language")
+if (language) {
+    getFeaturedRepos(language);
 
+    //clear old content
+    repoContainerEl.textContent = "";
+}
+}
+
+languageButtonEl.addEventListener("click", buttonClickHandler);
 userFormEl.addEventListener("submit", formSubmitHandler);
